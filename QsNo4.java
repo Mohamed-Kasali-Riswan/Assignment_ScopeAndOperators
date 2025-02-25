e1b4c33e65dbd0ed32ff86dc609c819525f4d186
Mohamed-Kasali-Riswan <146856255+Mohamed-Kasali-Riswan@users.noreply.github.com>
2025-02-25 17:39:05 +0530

    Update QsNo4.java

--- QsNo4.java
+++ QsNo4.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 // Write a program with an if statement. Declare one variable inside the if block and another one outside.
 // Print both variables inside and outside the if block to understand variable scope. 
 
-// Answer: Duplicate local variable b at scopesAndVariables
+// Answer: (Compile Time Error) Duplicate local variable b at scopesAndVariables
 
 public class QsNo4 {
 	public static void main(String[] args) {		
